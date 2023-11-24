6d658134eebdfad1d4122a8e054122d3d9c13f9d
Magicking <s@6120.eu>
2023-11-24 21:03:33 +0100

    Set static generation

--- svelte.config.cjs
+++ svelte.config.cjs
@@ -1,12 +1,19 @@
-import adapter from "@sveltejs/adapter-auto";
-import { vitePreprocess } from "@sveltejs/kit/vite";
+//import adapter from "@sveltejs/adapter-auto";
+import adapter from '@sveltejs/adapter-static';
+//import { vitePreprocess } from "@sveltejs/kit/vite";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter(),
+    adapter: adapter({
+		pages: 'build',
+		assets: 'build',
+		fallback: null,
+		precompress: false,
+		strict: true
+	}),
   },
-  preprocess: vitePreprocess(),
+  //preprocess: vitePreprocess(),
 };
 
 export default config;
