6009811f2f58ec9c75ce73aa222ee20d3976f8db
NoobDeveloper412 <mzuhair412@gmail.com>
2023-12-03 23:18:00 +0500

    Added some stuff

--- svelte.config.js
+++ svelte.config.js
@@ -4,16 +4,16 @@ import { vitePreprocess } from "@sveltejs/kit/vite";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  kit: {
-    adapter: adapter({
-		pages: 'build',
-		assets: 'build',
-		fallback: null,
-		precompress: false,
-		strict: true
-	}),
-  },
-  preprocess: vitePreprocess(),
+	kit: {
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+			precompress: false,
+			strict: true
+		}),
+	},
+	preprocess: vitePreprocess(),
 };
 
 export default config;
