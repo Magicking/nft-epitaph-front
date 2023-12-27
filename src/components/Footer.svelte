c27289101315ed34bbf35e5c3713cd6466528e6b
Magicking <s@6120.eu>
2023-12-28 00:40:55 +0100

    Fix a bunch

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,7 +1,7 @@
 <footer class="flex flex-col items-center py-8 bg-black text-white footer">
   <h3><a href="/">RG Epitaph</a></h3>
   <div class="w-full flex items-center justify-end mt-2">
-    <h5 class="author-text ">Reapers Gambit DAO</h5>
+    <h5 class="author-text "><a href="https://etherscan.io/address/0x89261878977B5a01C4fD78Fc11566aBe31BBc14e">Reapers Gambit DAO</a></h5>
   </div>
 </footer>
 
