fb06a0e22484c0590e756b9704e74d0927ff809f
Sylvain Laurent <s@6120.eu>
2023-12-27 18:46:25 +0100

    Update src/components/Footer.svelte

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,5 +1,5 @@
 <footer class="flex flex-col items-center py-8 bg-black text-white footer">
-  <h3><a href="/">Reapers GE</a></h3>
+  <h3><a href="/">RG Epitaph</a></h3>
   <div class="w-full flex items-center justify-end mt-2">
     <h5 class="author-text ">Made with ❤️ by Zuhayr</h5>
   </div>
