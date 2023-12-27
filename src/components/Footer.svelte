841ae745e058cde9f8488cd2618b3da044712a36
Sylvain Laurent <s@6120.eu>
2023-12-27 18:48:35 +0100

    Update src/components/Footer.svelte

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,7 +1,7 @@
 <footer class="flex flex-col items-center py-8 bg-black text-white footer">
   <h3><a href="/">RG Epitaph</a></h3>
   <div class="w-full flex items-center justify-end mt-2">
-    <h5 class="author-text ">Made with ❤️ by Zuhayr</h5>
+    <h5 class="author-text ">Reapers Gambit DAO</h5>
   </div>
 </footer>
 
