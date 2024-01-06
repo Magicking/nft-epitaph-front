8c6c006c8328da710d24f202153ed7789ab3f15e
Magicking <s@6120.eu>
2024-01-06 12:46:36 +0100

    Fix gateway

--- src/routes/mint/+page.svelte
+++ src/routes/mint/+page.svelte
@@ -15,7 +15,6 @@
   import rgeAbi from "$lib/rge.abi.json";
   import { fade } from "svelte/transition";
 
-
   let fUpdatePrice;
   const maxX = 128;
   const maxY = 24;
@@ -44,17 +43,19 @@
 
   onMount(async () => {
   const nym = createNymMixnetClient().then((nym) => {
-	  const nymApiUrl = 'https://validator.nymtech.net/api';
-
 	  // show message payload content when received 
 	  nym.events.subscribeToTextMessageReceivedEvent((e) => {
 		console.log('Got a message: ', e.args.payload);
 	  });
 
 	  // start the client and connect to a gateway
+	  let clientId = 'insecure';
+	  if (window.isSecureContext) {
+	   clientId = crypto.randomUUID();
+	  }
 	  nym.client.start({
-		clientId: crypto.randomUUID(),
-      	nymApiUrl,
+		clientId: clientId,
+      	preferredGateway: 'E3mvZTHQCdBvhfr178Swx9g4QG3kkRUun7YnToLMcMbM', //gateway1.nymtech.net
       	forceTls: true, // force WSS
 	  }).then((e) => {
 	  window.nym = nym;
